5f4c9b721d792b84f9bba44f4e66f2fd70bc3d45
bmatea <mateaboban05@gmail.com>
2020-12-03 23:30:46 +0100

    End of Section 17...

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,6 +4,7 @@ import { ReplaySubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { User } from '../_models/user';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class AccountService {
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private presence: PresenceService) { }
 
   login(model: any) {
     //anything we put in pipe will be an rxjs operator
@@ -23,16 +24,18 @@ export class AccountService {
         const user = response;
         if (user) {
           this.setCurrentUser(user);
+          this.presence.createHubConnection(user);
         }
       })
-    );
-  }
+      );
+    }
 
-  register(model: any) {
-    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
-      map((user: User) => {
-        if (user) {
-          this.setCurrentUser(user);
+    register(model: any) {
+      return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+        map((user: User) => {
+          if (user) {
+            this.setCurrentUser(user);
+            this.presence.createHubConnection(user);
         }
       })
     );
@@ -41,6 +44,7 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(undefined);
+    this.presence.stopHubConnection();
   }
 
   setCurrentUser(user: User) {
